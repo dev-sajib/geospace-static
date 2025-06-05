1920b8ecebb876efca58a9aeaa5fae57f282f818
FardinHash <iamfardin1526@gmail.com>
2025-06-05 11:25:43 +0600

    FEAT: Completed Press release page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import ClientsPage from "./pages/ClientsPage";
+import PressReleasePage from "./pages/PressReleasePage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/clients" element={<ClientsPage />} />
+          <Route path="/press-releases" element={<PressReleasePage />} />
         </Routes>
       </Layout>
     </Router>
